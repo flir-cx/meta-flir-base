882620372fd1dba53fa95b4821f8ca2f9567e4a5
Felix Hammarstrand <felix.hammarstrand@flir.se>
2019-05-14 10:39:44 +0200

    u-boot scripts: select sherlock-a dtb or sherlock dtb based on mainboard revision

--- recipes-bsp/u-boot/u-boot-script/ec201/select-fdt.sh
+++ recipes-bsp/u-boot/u-boot-script/ec201/select-fdt.sh
@@ -1,8 +1,17 @@
 
 
 setenv fdt_file_recovery      imx7ulp-sherlock.dtb;
-setenv fdt_file_sherlock      imx7ulp-sherlock.dtb;  #sherlock
-setenv fdt_file_detcal        imx7ulp-sherlock.dtb;  #chicken grill, detector calibration board
+setenv fdt_file_sherlock      imx7ulp-sherlock.dtb;  #ec201
+setenv fdt_file_sherlock_a    imx7ulp-sherlock-a.dtb;  #ec201 rev a
 
 setenv fdt_file ${fdt_file_sherlock};
 
+board main;
+
+if test $? != 0; then
+    setenv fdt_file ${fdt_file_sherlock};
+elif test ${main_board_revision} = 1; then
+      setenv fdt_file ${fdt_file_sherlock_a};
+else
+    setenv fdt_file ${fdt_file_sherlock};
+fi;
