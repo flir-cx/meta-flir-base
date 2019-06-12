80819b4ff032ecbb27f90fbf9ed4825f94864c30
Erik Bengtsson <erik.bengtsson@flir.se>
2019-06-12 09:55:25 +0200

    SHLK-886 SHLK-940 Add sysfs links for sherlock fuel guage and tp-leds

--- recipes-bsp/flir-sysfs-links/files/set-sysfs-links.sh
+++ recipes-bsp/flir-sysfs-links/files/set-sysfs-links.sh
@@ -20,8 +20,17 @@ lnk_base_dir=/etc/sysfs-links
 
 # Created symlinks
 lcd2dp_sink_caps_lnk=$lnk_base_dir/lcd2dp_sink_caps
+
 usbc_control_lnk=$lnk_base_dir/usbc_control
 usb2_control_lnk=$lnk_base_dir/usb2_control
+
+battery_lnk=$lnk_base_dir/battery
+
+tpleds_folder=$lnk_base_dir/tp-leds
+tpleds_detect_lnk=$lnk_base_dir/tp-leds/tp-leds
+tpled1_lnk=$lnk_base_dir/tp-leds/tp-led1
+tpled2_lnk=$lnk_base_dir/tp-leds/tp-led2
+tpled3_lnk=$lnk_base_dir/tp-leds/tp-led3
 
 find_out_model () {
 	grep -q -- '-evco' $compat_path && return $evco
@@ -51,18 +60,33 @@ set_paths () {
 		;;
 	"$bblc"|"$ec201")
 		usb2_control_path=/sys/bus/platform/drivers/ci_hdrc/ci_hdrc.0/udc/ci_hdrc.0
+		battery_path=/sys/class/power_supply/battery
+		tpleds_detect_path=/sys/bus/i2c/devices/1-0032
+		tpled1_path=/sys/class/leds/tp-led1
+		tpled2_path=/sys/class/leds/tp-led2
+		tpled3_path=/sys/class/leds/tp-led3
 		;;
 	esac
 }
 
+create_link() {
+	[ -n "$1" -a ! -L $2 ] && \
+		ln -sf $1 $2
+}
+
 create_links () {
 	mkdir -p $lnk_base_dir
-	[ -n "$lcd2dp_sink_caps_path" -a ! -L $lcd2dp_sink_caps_lnk ] && \
-		ln -sf $lcd2dp_sink_caps_path $lcd2dp_sink_caps_lnk
-	[ -n "$usbc_control_path" -a ! -L $usbc_control_lnk ] && \
-		ln -sf $usbc_control_path $usbc_control_lnk
-	[ -n "$usb2_control_path" -a ! -L $usb2_control_lnk ] && \
-		ln -sf $usb2_control_path $usb2_control_lnk
+	create_link "$lcd2dp_sink_caps_path" "$lcd2dp_sink_caps_lnk"
+	create_link "$usbc_control_path" "$usbc_control_lnk"
+	create_link "$usb2_control_path" "$usb2_control_lnk"
+
+	create_link "$battery_path" "$battery_lnk"
+	[ -n "$tpleds_detect_path" ] && \
+		mkdir $tpleds_folder
+	create_link "$tpleds_detect_path" "$tpleds_detect_lnk"
+	create_link "$tpled1_path" "$tpled1_lnk"
+	create_link "$tpled2_path" "$tpled2_lnk"
+	create_link "$tpled3_path" "$tpled3_lnk"
 }
 
 find_out_model
