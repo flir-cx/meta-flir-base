8148d708aed1e9fc2d32a85fd0baef18d2b48f00
David Sernelius <david.sernelius@flir.se>
2019-11-28 15:58:36 +0100

    SDK version of videorender

---
+++ recipes-flir/videorender/files/videorender.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# Need to source the needed XDG_* env
+source /etc/profile.d/weston.sh
+
+VIDEORENDER=/usr/bin/videorender
+VR_INPUT_DEVICES=""
+
+# Detect what IR and Visual sources are available
+VIS_DEVICE=$(find /dev/v4l -name "*viu-video-index0")
+IR_DEVICE=$(find /dev/v4l -name "*rpmsg-video-index0")
+
+if [ ! -z "${VIS_DEVICE}" ]
+then
+    VR_INPUT_DEVICES="${VR_INPUT_DEVICES} --input device=$(realpath "${VIS_DEVICE}"),type=visible,pixelformat=YUYV,width=640,height=480"
+fi
+
+if [ ! -z "${IR_DEVICE}" ]
+then
+    VR_INPUT_DEVICES="${VR_INPUT_DEVICES} --input device=$(realpath "${IR_DEVICE}"),type=colorized-ir,pixelformat=YUYV,width=160,height=120"
+fi
+
+VIDEORENDER_ARG="$VR_INPUT_DEVICES"
+
+echo "Executing: $VIDEORENDER $VIDEORENDER_ARG"
+exec $VIDEORENDER $VIDEORENDER_ARG
