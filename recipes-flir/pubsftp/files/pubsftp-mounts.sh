39a34594091333160e2e6ea4ce4f9c75f1c3a6e0
Ulf Palmér <ulf.palmer@flir.com>
2021-01-13 15:13:36 +0100

    sftp: make sure that /FLIR/images is chroot-able [SHLK-2583]
    (Note that we must wait with chmod until /FLIR/images is mounted
    by FLIR-images.mount)

--- recipes-flir/pubsftp/files/pubsftp-mounts.sh
+++ recipes-flir/pubsftp/files/pubsftp-mounts.sh
@@ -9,6 +9,13 @@
 ## Check for expected environment
 [ -d /srv/sftp ] || mkdir -m 0755 -p /srv/sftp
 
+## Make sure that /FLIR/images is chroot-able
+ls -ld /FLIR/images | grep -q drwxr-xr-x
+if [ $? -ne 0 ]
+then
+    echo "Make /FLIR/images chroot-able: chmod 0755 /FLIR/images"
+    chmod 0755 /FLIR/images
+fi
 ## Add bind-mounts
 mount --bind /FLIR/images /srv/sftp
 
