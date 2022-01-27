332fc6d4d1ecea33638c463ff53fbfa3911a24a5
Mathias Båge <mathias.bage@flir.com>
2022-01-27 18:32:33 +0100

    HABA-259 BLE discovery service & script
    Uses hcitool to turn on (and off) the broadcasting
    of SSID and faked camera serial number (32-bit in
    network order).
    
    ble-discovery.sh:
    * improved error checks
    * little endian serial number
    * no Beacon type and TX power

---
+++ recipes-connectivity/ble-discovery/ble-discovery.bb
@@ -0,0 +1,26 @@
+SUMMARY = "BLE discovery"
+DESCRIPTION = "Service (oneshot) and script for BLE discovery"
+AUTHOR = "Mathias Båge <mathias.bage@flir.com>"
+SECTION = "flir/applications"
+PRIORITY = "optional"
+LICENSE = "CLOSED"
+PR = "r0"
+
+inherit systemd
+
+RPROVIDES_${PN} += "${PN}-systemd"
+RREPLACES_${PN} += "${PN}-systemd"
+RCONFLICTS_${PN} += "${PN}-systemd"
+SYSTEMD_SERVICE_${PN} = "ble-discovery.service"
+
+SRC_URI += "file://ble-discovery.sh"
+SRC_URI += "file://ble-discovery.service"
+
+S = "${WORKDIR}/"
+
+do_install_append() {
+    install -d ${D}${systemd_unitdir}/system
+    install -m 0644 ${S}/ble-discovery.service ${D}${systemd_unitdir}/system
+    install -d ${D}/sbin/
+    install -m 0744 ${S}/ble-discovery.sh ${D}/sbin/ble-discovery.sh
+}
---
+++ recipes-connectivity/ble-discovery/files/ble-discovery.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# BLE 4.0 Legacy
+
+/usr/bin/hcitool -i hci0 cmd 0x08 0x000a 00
+
+test "$1" = off && exit
+
+if [ ! -r /etc/wpa_supplicant.conf ]
+then
+    echo "error: /etc/wpa_supplicant.conf missing - can't get SSID - Beacon unchanged"
+    /usr/bin/hcitool -i hci0 cmd 0x08 0x000a 01
+    exit 1
+fi
+
+eval $(grep ssid= /etc/wpa_supplicant.conf | xargs echo -n)
+
+# SSID maximum length is 18 chars
+if [ -n "$ssid" ]
+then
+    if [ ${#ssid} -gt 18 ]
+    then
+	echo "error: SSID too long (${#ssid} characters) - Beacon unchanged"
+	/usr/bin/hcitool -i hci0 cmd 0x08 0x000a 01
+	exit 1
+    fi
+    SSID=$ssid
+else
+    echo "error: can't get ssid from /etc/wpa_supplicant.conf"
+    /usr/bin/hcitool -i hci0 cmd 0x08 0x000a 01
+    exit 1
+fi
+
+# fake serial number
+CAMSERIAL=894000764
+# 32-bit
+CAMSERIAL_HEX=$(printf "%08X" $CAMSERIAL)
+
+# little endian
+CAMSERIAL_HEX_BYTES="${CAMSERIAL_HEX:6:2} ${CAMSERIAL_HEX:4:2} ${CAMSERIAL_HEX:2:2} ${CAMSERIAL_HEX:0:2}"
+SSID_HEX_BYTES=$(echo -n "$SSID" | hexdump -e '32/1 "%02X "')
+SSID_LEN=${#SSID}
+
+while [ $SSID_LEN -lt 18 ]
+do
+    SSID_HEX_BYTES+=" 00"
+    SSID_LEN=$((SSID_LEN+1))
+done
+
+FLAGS="02 01 06"
+LENGTH=1A
+TYPE=FF
+COMPANY_ID="E9 04"   # little endian
+
+# note: 1st time setup bit (bit 0) not set yet
+if [ -a /etc/skylab/passkey-changed ]
+then
+    FLIR_FLAGS=02
+else
+    FLIR_FLAGS=00
+fi
+
+# allows for longer SSIDs
+BEACON_TYPE_EMPTY=""
+
+/usr/bin/hcitool -i hci0 cmd 0x08 0x0008 1E $FLAGS $LENGTH $TYPE $COMPANY_ID $BEACON_TYPE_EMPTY $FLIR_FLAGS $CAMSERIAL_HEX_BYTES $SSID_HEX_BYTES 00
+
+/usr/bin/hcitool -i hci0 cmd 0x08 0x000a 01
