529834fa4e337057a866ef1b5c571553e2089f6c
Ola Redell <ola.redell@teledyneflir.com>
2022-03-01 09:59:03 +0100

    [BC-56] btload: Fix start ordering issues
    * btload needs to enable bluetooth using connmanctl, not rfkill, after
      connman has started, otherwise connman overrides and disables bluetooth
      when it starts (https://wiki.archlinux.org/title/ConnMan#Technologies)
    
    * btload.service also needs to start after bluetooth.service, with a 2 sec
      delay, otherwise we see failures such as "Failed to set privacy: Rejected
      (0x0b)" in the logs from bluetooth.service, see
      https://github.com/RPi-Distro/pi-bluetooth/issues/8

--- recipes-connectivity/btload/files/evco/btload.sh
+++ recipes-connectivity/btload/files/evco/btload.sh
@@ -2,22 +2,8 @@
 
 set -e
 
-SCRIPTNAME="$(basename ${0})"
-
-echo "Loading Bluetooth device driver"
-
-# Bluetooth MAC is stored in radio module
-
-modprobe hci_uart
-
-uim -f /sys/bus/platform/drivers/kim/kim.* &
-
-modprobe btwilink
-
-# Need to rest a while before unblocking
-sleep 1
-rfkill unblock bluetooth
-
+connmanctl enable bluetooth
+ 
 if hciconfig hci0 up; then
 # SCO packets shall be sent via HCI interface
 # SCO packet size must be 120 bytes according to TI to align with 2-EV3 packet
@@ -29,4 +15,3 @@ else
   logger -p error -s "Bluetooth driver load failed"
   echo "Bluetooth driver load failed"
 fi
-
