ae9bb86fef3c8e23b92e01f5b3e9cede29e4c1ce
Erik Bengtsson <Erik.Bengtsson@teledyneflir.com>
2022-06-28 08:26:43 +0200

    camserial: HABA-668 should be 9 chars
    Serial number is 9 chars, no need to print out 10. Prints out strange
    character on yocto 3.3

--- recipes-flir/flirmisc/files/camserial.sh
+++ recipes-flir/flirmisc/files/camserial.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-SERIAL_FF=$(printf "\\xFF\\xFF\\xFF\\xFF\\xFF\\xFF\\xFF\\xFF\xFF\xFF" | hexdump -n10 -e '10/1 "%c"')
+SERIAL_FF=$(printf "\\xFF\\xFF\\xFF\\xFF\\xFF\\xFF\\xFF\xFF\xFF" | hexdump -n9 -e '9/1 "%c"')
 
-SERIAL=$(hexdump -s36 -n10  /sys/devices/platform/40800000.ahb-bridge1/40a40000.lpi2c6/i2c-1/1-0057/eeprom -e '10/1 "%c"' | tr -d '\0')
+SERIAL=$(hexdump -s36 -n9  /sys/devices/platform/40800000.ahb-bridge1/40a40000.lpi2c6/i2c-1/1-0057/eeprom -e '9/1 "%c"' | tr -d '\0')
 
 if [ "$SERIAL" = "$SERIAL_FF" ] || [ "$SERIAL" = "*" ] || [ "$SERIAL" = "" ]; then
   echo "*"
