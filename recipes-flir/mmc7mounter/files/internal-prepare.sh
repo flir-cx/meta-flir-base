b423c32672a7d94fd6fa06255283109b3a42b702
Ulf Palmér <ulf.palmer@teledyneflir.com>
2022-09-22 10:08:48 +0200

    mmc7mounter: internal_prepare do more thorough check on mmcblk0p7
    mmcblk0p7 is intended (in yocto3.3) to be used as "internal" storage
    For earlier versions of the code, this partition has been unused ("spare")
    
    We now check that:
    1. p7 has the expected label
    2. that p7 is correctly ext4 formatted
    If not, re-prepare p7

--- recipes-flir/mmc7mounter/files/internal-prepare.sh
+++ recipes-flir/mmc7mounter/files/internal-prepare.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 
 set -e
+prepare_p7=
 
 if (fdisk -l /dev/mmcblk0 | grep -q storage)
 then
     echo "`basename $0`: /dev/mmcblk0p7 is correctly (re-)labeled"
-else    
+else
+    echo "`basename $0`: /dev/mmcblk0p7 is never used (old style)"
+    prepare_p7="yes"
+fi
+
+if (fsck.ext4 /dev/mmcblk0p7 -n 2>/dev/null 1>/dev/null)
+then
+    echo "`basename $0`:/dev/mmcblk0p7 has a valid ext4 file system"
+else
+    # ext4 not OK
+    echo "`basename $0`:/dev/mmcblk0p7 needs formatting"
+    prepare_p7="yes"
+fi
+
+if [ -n "$prepare_p7" ]
+then
+    echo "`basename $0`:prepares p7 partition"
     echo "`basename $0`: mke2fs -t ext4 /dev/mmcblk0p7 -E nodiscard"
     mke2fs -t ext4 /dev/mmcblk0p7 -E nodiscard -F
-    echo "mke2fs done"
+    echo "`basename $0`:mke2fs done"
     echo "`basename $0`:renames partition mmcblk0p7 to storage" 
     parted /dev/mmcblk0 name 7 storage
 fi
