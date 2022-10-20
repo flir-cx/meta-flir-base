13f7b678240f055a88e1140be56b073095d50d89
Ulf Palmér <ulf.palmer@teledyneflir.com>
2022-10-20 13:45:34 +0200

    camserial: now works on both imx7 based products and evco/eoco

--- recipes-flir/flirmisc/files/mx6/camserial.sh
+++ recipes-flir/flirmisc/files/mx6/camserial.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# mx7 version
+# evco/eoco version
 
 SERIAL_FF=$(printf "\\xFF\\xFF\\xFF\\xFF\\xFF\\xFF\\xFF\xFF\xFF" | hexdump -n9 -e '9/1 "%c"')
 
-SERIAL=$(hexdump -s36 -n9  /sys/devices/platform/40800000.ahb-bridge1/40a40000.lpi2c6/i2c-1/1-0057/eeprom -e '9/1 "%c"' | tr -d '\0')
+SERIAL=$(hexdump -s36 -n9 /sys/devices/platform/soc/2100000.bus/21a8000.i2c/i2c-2/2-0057/eeprom -e '9/1 "%c"' | tr -d '\0')
 
 if [ "$SERIAL" = "$SERIAL_FF" ] || [ "$SERIAL" = "*" ] || [ "$SERIAL" = "" ]; then
   echo "*"
