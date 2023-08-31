7b5c29a97639e3938854f3e3c768655b1f9578f9
Ulf Palmér <ulf.palmer@teledyneflir.com>
2023-08-31 09:45:30 +0200

    progressapp_weston: stay alive after start [EXXT5T8XX-7465]
    We remove special behaviour (to save memory consumption) for
    mx6 (evco)
    If progressapp was stopped, at wakeup from sleep,
    briefly there will be checkered pattern, which is not looking good
    With progressapp, the FLIR logo will be shown
    
    low free memory is to be handled by other commits (zram swap)

--- recipes-flir/progressapp_weston/files/mx6/progressapp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# Need to source the needed XDG_* env
-source /etc/profile.d/weston.sh
-
-PROGRESSAPP="/FLIR/usr/bin/progressapp_weston -k"
-
-echo "Executing: $PROGRESSAPP"
-exec $PROGRESSAPP
