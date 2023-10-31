7539908f04846f7e8bfe61f663cda925143432e4
Jonas Rydow <jonas.rydow@teledyneflir.com>
2023-10-31 14:02:42 +0100

    Change to performance governor on startup.
    This gives somewhere between 800ms to 1 second.
    
    Cherry picked from 354af60268f97df464771825bbf37f42e0a51b00

--- recipes-flir/appcore/files/flirapp_reduce_speed.sh
+++ recipes-flir/appcore/files/flirapp_reduce_speed.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 # evco: reduce speed ~30s after flirapp start to avoid overheating
-sleep 30
+sleep 28
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+sleep 2
+echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
 
 echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo "flirapp: now run with reduced CPU max speed"
