d9e3ffe77be2a966ed5463fea3f9fb358b616f92
Erik Bengtsson <erik.bengtsson@teledyneflir.com>
2023-12-19 11:33:56 +0100

    flirapp: CXX-466 wait for videorender
    Wait for videorender to start streams before starting appcore,
    mismatch can occur if videorender sets resolution of stream after
    m4 device in appcore is initialized.

--- recipes-flir/appcore/files/ec302/flirapp_env_check.sh
+++ recipes-flir/appcore/files/ec302/flirapp_env_check.sh
@@ -1,8 +1,26 @@
 #!/bin/bash -e
 
+declare -i count=0
+
 echo 512 > /proc/sys/fs/mqueue/msgsize_max
 echo 80 > /proc/sys/fs/mqueue/msg_max
 
 rm -rf `ls /tmp/FLIRevent/* | grep -v Progress`
 rm -rf `ls /dev/mqueue/* | grep -v Progress`
+
+# Wait until videorender started streams
+while true; do	
+	if [ $count -gt 10 ]; then
+		exit 0
+	fi
+
+	if journalctl -u videorender | grep -q "Have all streams"; then
+		echo "Videorender ready"
+		exit 0
+	fi
+
+	echo "Wait for Videorender..."
+	sleep 0.5
+	count=$count+1
+done
 
