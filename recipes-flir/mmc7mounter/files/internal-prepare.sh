2558c2467d701122df5ff342f8af89bbbcec9aff
Jonas Rydow <jonas.rydow@teledyneflir.com>
2024-02-02 10:48:10 +0100

    BC-651 Only run internal-prepare after update
    - Remove superfluous shell spawning in internal-prepare.
    - Add ConditionNeedUpdate to only trigger script after updates.
    - Change to After local-fs.target to avoid circular references.
    - In total this gives somewhere between 50 and 100ms

--- recipes-flir/mmc7mounter/files/internal-prepare.sh
+++ recipes-flir/mmc7mounter/files/internal-prepare.sh
@@ -2,41 +2,42 @@
 
 set -e
 prepare_p7=
+bn=`basename $0`
 
 if (busybox.nosuid fdisk -l /dev/mmcblk0 | grep -q storage)
 then
-    echo "`basename $0`: /dev/mmcblk0p7 is correctly (re-)labeled"
+    echo "$bn: /dev/mmcblk0p7 is correctly (re-)labeled"
 else
-    echo "`basename $0`: /dev/mmcblk0p7 is never used (old style)"
+    echo "$bn: /dev/mmcblk0p7 is never used (old style)"
     prepare_p7="yes"
 fi
 
 if (fsck.ext4 /dev/mmcblk0p7 -n 2>/dev/null 1>/dev/null)
 then
-    echo "`basename $0`:/dev/mmcblk0p7 has a valid ext4 file system"
+    echo "$bn:/dev/mmcblk0p7 has a valid ext4 file system"
 else
     # ext4 not OK
-    echo "`basename $0`:/dev/mmcblk0p7 needs formatting"
+    echo "$bn:/dev/mmcblk0p7 needs formatting"
     prepare_p7="yes"
 fi
 
 if [ -n "$prepare_p7" ]
 then
-    echo "`basename $0`:prepares p7 partition"
-    echo "`basename $0`: mke2fs -t ext4 /dev/mmcblk0p7 -E nodiscard"
+    echo "$bn:prepares p7 partition"
+    echo "$bn: mke2fs -t ext4 /dev/mmcblk0p7 -E nodiscard"
     mke2fs -t ext4 /dev/mmcblk0p7 -E nodiscard -F
-    echo "`basename $0`:mke2fs done"
-    echo "`basename $0`:renames partition mmcblk0p7 to storage" 
+    echo "$bn:mke2fs done"
+    echo "$bn:renames partition mmcblk0p7 to storage" 
     parted /dev/mmcblk0 name 7 storage
 fi
 
 if [ ! -h /FLIR/images ]
 then
     # not a symbolic link...
-    echo "`basename $0`:resets /FLIR/images to use internal"
+    echo "$bn:resets /FLIR/images to use internal"
 
     rm -rf /FLIR/images
     ln -sf internal /FLIR/images
 else
-    echo "`basename $0`: /FLIR/images uses internal"
+    echo "$bn: /FLIR/images uses internal"
 fi
