40059a8074ca59e2b401ea628c9cf04006fe06c0
Klas Malmborg <klas.malmborg@teledyneflir.com>
2024-02-12 06:43:53 +0100

    BC-562 Add serial to USB descriptor

--- recipes-connectivity/gadgetload/files/gadget.sh
+++ recipes-connectivity/gadgetload/files/gadget.sh
@@ -15,6 +15,8 @@ usbmode_uvc=false
 # Device info
 product="FLIR Camera" # TODO: Populate with product name
 manufacturer="FLIR Systems"
+serialno=$(camserial)
+serialno=${serialno:=*}
 vendor_id="0x09CB"
 product_id=""
 product_id_rndis="0x1002"
@@ -221,6 +223,10 @@ config_load() {
 	mkdir -p strings/0x409
 	echo "${manufacturer}"  > strings/0x409/manufacturer
 	echo "${product}"	    > strings/0x409/product
+	if [[ $usbmode == "UVC_MTP" ]]
+	then
+		echo "${serialno}" > strings/0x409/serialnumber
+	fi
 
 	# Configuration Descriptor, config 1.
 	mkdir -p configs/c.1
