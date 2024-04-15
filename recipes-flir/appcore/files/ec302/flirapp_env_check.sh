dabfb5fd35fad7db7c59af7526ca8b0729a4fc3b
Jonas Rydow <jonas.rydow@teledyneflir.com>
2024-04-15 08:41:53 +0200

    BC-676 Remove bash invocation and send errors to /dev/null
    - Send error message if directory not found to /dev/null
    - Remove on bash invocation and one ls invocation

--- recipes-flir/appcore/files/ec302/flirapp_env_check.sh
+++ recipes-flir/appcore/files/ec302/flirapp_env_check.sh
@@ -9,8 +9,7 @@ declare -i count=0
 echo 512 > /proc/sys/fs/mqueue/msgsize_max
 echo 80 > /proc/sys/fs/mqueue/msg_max
 
-rm -rf `ls /tmp/FLIRevent/* | grep -v Progress`
-rm -rf `ls /dev/mqueue/* | grep -v Progress`
+rm -rf $(ls /tmp/FLIRevent/* /dev/mqueue/* 2>/dev/null | grep -v Progress)
 
 # Wait until videorender started streams
 while true; do	
