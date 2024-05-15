dc8be5073455d6cba84cf27d85e82a8272995af1
Ulf Palmér <ulf.palmer@teledyneflir.com>
2024-05-15 08:25:39 +0200

    wlanload, load wlan driver "late" [BC-741]
    To get better perceived boot time, we delay "unnecessary" services
    (like wi-fi) to after flirapp startup
    Implemented here for "all" FLIR targets except ec401w (S21/"habanero")
    and eoco (Gxxx/"eowyn")
    
    For included targets (evco, ec501, ec302, ec201):
    - wi-fi driver is blacklisted to avoid auto init by systemd
      (for the selected targets)
    - wlanload.service will run on all targets
      for selected ones, marked as "after" flir-basic.target
      to run it "late"
    - wlanload will now load driver and trig connman to reload its config
    - (wlanload also handles "noradio option" where wlan driver has been
      "deleted")
    
    Notes (other targets):
    ec401w: keeps its previous setup as of now
    eoco: keeps its previous setup as of now
    ec702 et al: wlanload not present in image as of now. Needs additional
    commit if wanted
    
    These (other) targets needs additional commit(s) if wlanload concept is
    to be changed here too

--- recipes-connectivity/wlanload/files/ec302/wlanload.sh
+++ recipes-connectivity/wlanload/files/ec302/wlanload.sh
@@ -1,28 +1,12 @@
 #!/bin/sh
 
-WLANADDR="$(sed -ne 's,^.*wlanaddr=\([^[:blank:]]\+\)[:blank:]*.*,\1,g;T;p' /proc/cmdline)"
-WLANADDR="$(echo "$WLANADDR" | sed 's/://g')"
-WLANADDR_BIN=/lib/firmware/wlan/qca9377/wlan_mac.bin
-WLANADDR_TMP=/tmp/wlanaddr
+echo "Starting wi-fi - iMX7/qca9377"
+# start wlan module (we use qcom builtin mac adress for now,
+# skipping cmdline mac)
+if ! modprobe qca9377; then echo no wi-fi driver; exit 1; fi
 
-if [ -z $WLANADDR ]; then
-    echo "No wlanaddr in cmdline"
-    exit 0
-fi
-
-echo "Intf0MacAddress=$WLANADDR
-Intf1MacAddress=00AA00BB00C2
-Intf2MacAddress=00AA00BB00C3
-Intf3MacAddress=00AA00BB00C4" > $WLANADDR_TMP
-
-
-if diff "$WLANADDR_BIN" "$WLANADDR_TMP" > /dev/null; then
-    rm -f $WLANADDR_TMP
-    exit 0 # addr same as in bin file
-fi
-
-mv $WLANADDR_TMP $WLANADDR_BIN
-
-#restart wlan module to set correct addr
-modprobe -r qca9377
-modprobe qca9377
+sleep 0.5
+# trig reload of connman to pick up wi-fi
+killall -USR1 connmand
+echo "wi-fi should now be set"
+exit 0
--- recipes-connectivity/wlanload/files/evco/wlanload.sh
+++ recipes-connectivity/wlanload/files/evco/wlanload.sh
@@ -1,32 +1,12 @@
 #!/bin/sh
 
-echo "Starting WiFi"
-set -e
+echo "Starting wi-fi - iMX6/wl18xx"
 
-# MAC addresses (from TI) are already present in the module
+if ! modprobe wl18xx; then echo no wi-fi driver; exit 1; fi
 
-# Load 'cfg80211' and let it settle down (needed by 'wlcore_sdio')
-modprobe -q cfg80211 && sleep 0.2
+sleep 0.5
 
-# wlcore_sdio.ko
-if ! grep -qs wlcore_sdio /proc/modules; then
-	RETRIES="2"
-	while [ "${RETRIES}" -gt "0" ]; do
-		modprobe --ignore-install -q wlcore_sdio || true
-		SLEEPLOOPS="6"
-		while [ "${SLEEPLOOPS}" -gt "0" ]; do
-			sleep 0.5
-			[ -d "/sys/class/net/wlan0" ] && break
-			SLEEPLOOPS="$((SLEEPLOOPS - 1))"
-		done
-		[ -d "/sys/class/net/wlan0" ] && break
-		RETRIES="$((RETRIES - 1))"
-		rmmod wlcore_sdio > /dev/null
-		echo "Retrying to load wireless"
-	done
-	[ "${RETRIES}" -eq "0" ] && echo "Loading wlcore_sdio module: [FAILED]"
-fi
-
-# Delay required for the interface 'wlan0' to settle down before trying to configure it.
-sleep 0.1
-#ifconfig wlan0 up
+# trig reload of connman to pick up wi-fi
+killall -USR1 connmand
+echo "wi-fi should now be set"
+exit 0
